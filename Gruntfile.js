cb63927eda9cc597e4fb4cf0a6f4bfa5f2fb4f6c
OttoRobotto <alexander.ottenhoff@gmail.com>
2014-12-04 12:23:56 +0100

    Fixed typo in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
       options: {
         //You can find these in your Shopify Admin under Apps then look at the bottom for Create a private API Key
         api_key: "fa8b1464d6dc5fcd6e0c9f51bde3fa3b",
-        password: "fb92deb0d63f29c5ef2949d4a88f6f0"e,
+        password: "fb92deb0d63f29c5ef2949d4a88f6f0e",
         url: "limemakers.myshopify.com",
         base: 'shop/',
         ThemeId: "8914385",
