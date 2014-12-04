8858d3912526af06ef0b1e65d77c09b64f797ff9
OttoRobotto <alexander.ottenhoff@gmail.com>
2014-12-04 12:10:43 +0100

    Initial commit of shop theme

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+ 
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-shopify');
+ 
+  grunt.initConfig({
+    shopify: {
+      options: {
+        //You can find these in your Shopify Admin under Apps then look at the bottom for Create a private API Key
+        api_key: "fa8b1464d6dc5fcd6e0c9f51bde3fa3b",
+        password: "fb92deb0d63f29c5ef2949d4a88f6f0"e,
+        url: "limemakers.myshopify.com",
+        base: 'shop/',
+        ThemeId: "8914385",
+        disable_growl_notifications: "true"
+      }
+    },
+    watch: {
+      shopify: {
+        files: ['shop/**'],
+        tasks: ["shopify"]
+      }
+    }
+ 
+  });
+ 
+  grunt.registerTask('default', ['shopify']);
+ 
+};
